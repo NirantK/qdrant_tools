a2fb3370f008366d961f0cda1361d254d803912a
NirantK <nirant.bits@gmail.com>
2023-06-08 16:13:29 +0530

    * refactor(pinecone_migration.py): change import statements to use qdrant_tools.vectordb instead of QdrantJourney and APIKeys classes. Remove api_keys parameter from PineconeExport and QdrantImport constructors.

--- examples/pinecone_migration.py
+++ examples/pinecone_migration.py
@@ -1,12 +1,9 @@
-from QdrantJourney import APIKeys, PineconeExport, QdrantImport
-
-keys = ["PINECONE_API_KEY", "PINECONE_ENVIRONMENT", "QDRANT_URL", "QDRANT_API_KEY"]
-api_keys = APIKeys(keys)
+from qdrant_tools.vectordb import PineconeExport, QdrantImport
 
 index_name = "example-index"  # Existing Pinecone index name
 
 # Init Pinecone
-pinecone_ex = PineconeExport(api_keys)
+pinecone_ex = PineconeExport()
 pinecone_index = pinecone_ex.create_index(
     index_name
 )  # Assuming the index already exists
@@ -22,6 +19,6 @@ vector_ids = ["1", "2", "3", "4", "5"]  # Example vector ids
 vector_dimension = pinecone_index.describe_index_stats()[
     "dimension"
 ]  # Get dimension from existing Pinecone index
-qdrant = QdrantImport(api_keys)
+qdrant = QdrantImport()
 qdrant.recreate_collection(index_name, vector_dimension)
 qdrant.upsert_vectors(index_name, vector_ids, pinecone_index)
