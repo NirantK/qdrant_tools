5338761ee97c5dddc8db46e76f5ee835346b08ab
NirantK <nirant.bits@gmail.com>
2023-06-08 15:23:27 +0530

    * chore(setup.py): update qdrant-client dependency to version 1.2.0 or higher * chore(setup.py): update package version to 0.0.1 * chore(setup.py): remove cohere dependency * chore(setup.py): remove package description

--- qdrant_tools/setup.py
+++ qdrant_tools/setup.py
@@ -2,14 +2,13 @@ from setuptools import setup
 
 setup(
     name="qdrant-journey",
-    version="0.1.0",
-    description="A sample package",
-    author="Aditya and Nirant",
+    version="0.0.1",
+    description="",
+    author="Nirant Kasliwal",
     author_email="nirant.bits@gmail.com",
     packages=["qdrant-journey"],
     install_requires=[
         "pinecone-client==2.2.1",
-        "qdrant-client==1.2.0",
-        "cohere==4.6.0",
+        "qdrant-client>=1.2.0",
     ],
 )
